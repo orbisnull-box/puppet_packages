dbd138ce1bc012f96b9384fc764e8ad6d11368f9
orbisnull <a1p2m3@gmail.com>
2016-12-05 06:36:17 +0800

    fix combine hiera data and self params

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,15 @@
 class orbisnull_packages::install inherits orbisnull_packages {
-  if $packages == undef {
-    $packages = hiera_array('orbisnull_packages::packages')
+  
+  $packages_hiera = hiera_array('orbisnull_packages::packages')
+  if $packages != undef {
+    $packages_full=concat($packages_hiera, $packages)
+  } 
+  else {
+    $packages_full = $packages_hiera
   }
-  package { $packages:
+  $packages_install = unique($packages_full)
+  notify{"packages: ${packages_install}":}
+  package { $packages_install:
     ensure => $ensure,
   }
 }
